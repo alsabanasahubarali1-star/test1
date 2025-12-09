92207606c208800a70af2378a931ac9556ace44a
alsabanasahubarali1-star <alsabana.sahubarali1@freshworks.com>
2025-12-10 02:37:28 +0530

    v2_chcek
    v2_chcek

---
+++ v2.js
@@ -0,0 +1,11 @@
+function add(a, b) {
+  return Number(a) + Number(b);  // changed logic
+}
+
+function getTimeout() {
+  return 60000; // 60 seconds (changed)
+}
+
+function newFeature() {
+  console.log("Feature added!");
+}
